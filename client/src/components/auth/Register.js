e430142de7398b920e50e8d049c3a08522a8ee48
DaraiSidorko <daria.sidorko@gmail.com>
2020-03-29 21:54:04 -0700

    UI update

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,7 +3,23 @@ import React, { Component } from 'react'
 class Register extends Component {
   render() {
     return (
-      <div className="register">
+      <div className="wrapper">
+      <div className="main-content">
+        <div className="header">
+          <img src={require("../../img/instagram_logo.png")} />
+        </div>
+        <input type="text" placeholder="Email"/>
+        <input type="text" placeholder="Full Name"/>
+        <input type="text" placeholder="Username"/>
+        <input type="password" placeholder="Password" />
+        <input type="button" value="Sign up" className="btn" />
+
+      </div>
+      <div className="sub-content">
+          Have an account? <a className="sub-link" href="#">Log in</a>
+      </div>
+    </div>
+/*       <div className="register">
       <div className="container">
         <div className="row">
           <div className="col-md-8 m-auto">
@@ -28,7 +44,7 @@ class Register extends Component {
           </div>
         </div>
       </div>
-    </div>
+    </div> */
     )
   }
 }
--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center">
-    Copyright &copy; {new Date().getFullYear()} Dev Connector
+    <footer className="footer text-center">
+    &copy; {new Date().getFullYear()} from Kal Academy students
   </footer>
   )
 }
