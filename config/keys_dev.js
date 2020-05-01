f72496b736be150c4f6237962feef9aa1e81a779
surryjm <50641921+surryjm@users.noreply.github.com>
2020-05-01 15:20:55 -0700

    Brought back keys

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: `mongodb+srv://surry:${encodeURIComponent("Pass123")}@instagramappkalacademy-n6ibo.mongodb.net/instagramdb?retryWrites=true&w=majority`,
+  secretOrKey: 'secret'
+};
